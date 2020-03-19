1c66dd335059e1700615b4bd73e59593806becde
Kioh Han <kioh.han@disney.com>
2020-03-18 21:29:52 -0400

    add swagger ui

---
+++ json-producer/src/main/java/com/kioh/kafka/jsonproducer/JsonproducerApplication.java
@@ -0,0 +1,13 @@
+package com.kioh.kafka.jsonproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonproducerApplication.class, args);
+	}
+
+}
--- simple-producer/src/main/java/com/kioh/kafka/simpleproducer/SimpleproducerApplication.java
+++ simple-producer/src/main/java/com/kioh/kafka/simpleproducer/SimpleproducerApplication.java
@@ -3,7 +3,10 @@ package com.kioh.kafka.simpleproducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SimpleproducerApplication {
 
 	public static void main(String[] args) {
